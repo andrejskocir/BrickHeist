546bf015aa6515b2495e082583ce82d9f32e499e
andrejskocir <andrej.skocir@gmail.com>
2022-04-07 22:37:15 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -14,10 +14,14 @@ const select = document.querySelector(".select")
 
 let btnCenter = title.offsetWidth/2 - start.offsetWidth/2
 console.log(btnCenter);
+console.log(title.offsetWidth);
+console.log(start.offsetWidth);
+
+
 title.addEventListener("mouseover", (e)=>{
     first.style.transform = "translateY(-35%)"
     second.style.transform = "translateY(35%)"
-    start.style.transform = "translateX("+`${btnCenter}`+"px) translateY(-5vh)"
+    start.style.transform = "translateX("+`${title.offsetWidth/2-start.offsetWidth/2}`+"px) translateY(-5vh)"
     start.style.transition = "750ms ease-in-out"
 });
 
@@ -25,7 +29,7 @@ title.addEventListener("mouseover", (e)=>{
 title.addEventListener("mouseleave", (e)=>{
     first.style.transform = "translateY(0%)"
     second.style.transform = "translateY(0%)"
-    start.style.transform = "translateX(-120vw) translateY(-5vh)"
+    start.style.transform = "translateX(-100vw) translateY(-5vh)"
     start.style.transition = "750ms ease-in-out"
 })
 
@@ -64,3 +68,22 @@ hard.addEventListener("click", ()=>{
 select.addEventListener("click", ()=>{
     
 })
+
+//Get the button
+var scrollToTop = document.querySelector(".scrollToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
